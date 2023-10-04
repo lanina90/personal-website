955a5ea537a3d5fdcec8430dfcd7182a73873ca3
Kateryna Laina <boba4ok@gmail.com>
2023-10-04 15:19:27 +0300

    commit

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,11 +2,20 @@ import React from 'react'
 import styles from './Header.module.css'
 import Container from '../Container/Container'
 import Logo from '../../assets/img/logo.svg'
+import {useMedia} from '../../hooks/useMedia';
 
 const Header = () => {
+  const {isMobile} = useMedia()
   return (
     <Container component="header" className={styles.logo}>
       <img src={Logo} width={'44px'} alt="Logo" />
+
+      <nav>
+        <a href="">About</a>
+        <a href="">Experience</a>
+        <a href="">Contacts</a>
+      </nav>
+
     </Container>
   )
 }
