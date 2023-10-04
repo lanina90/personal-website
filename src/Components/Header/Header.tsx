1e5eb17d0b6e9252114ae48714d37c13a38c9f56
Kateryna Laina <boba4ok@gmail.com>
2023-10-04 11:03:11 +0300

    feature: setup eslint and prettier

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from 'react'
 import styles from './Header.module.css'
-import Container from "../Container/Container";
+import Container from '../Container/Container'
 
 const Header = () => {
   return (
     <Container component="header" className={styles.logo}>
-      <img src='/img/logo.svg' width={'44px'} alt=""/>
+      <img src="/img/logo.svg" width={'44px'} alt="" />
       <p className={styles.logo__name}>Kateryna Lanina</p>
     </Container>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import Cube from "../Cube/Cube";
+import React from 'react'
+import Cube from '../Cube/Cube'
 import styles from './Hero.module.css'
-import Container from "../Container/Container";
+import Container from '../Container/Container'
 
 const Hero = () => {
   return (
     <Container component={'section'} className={styles.hero}>
-        <div className={styles.hero__intro}>
-          <h1>I'm a front developer</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid, amet culpa distinctio dolorem eligendi, et eveniet fuga libero nihil numquam obcaecati qui quibusdam quisquam reprehenderit sint ullam unde voluptatum?
-            Accusamus aspernatur beatae culpa deleniti labore magnam maxime mollitia pariatur recusandae sequi.
-          </p>
-        </div>
-        <Cube/>
+      <div className={styles.hero__intro}>
+        <h1>I'm a front developer</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid,
+          amet culpa distinctio dolorem eligendi, et eveniet fuga libero nihil
+          numquam obcaecati qui quibusdam quisquam reprehenderit sint ullam unde
+          voluptatum? Accusamus aspernatur beatae culpa deleniti labore magnam
+          maxime mollitia pariatur recusandae sequi.
+        </p>
+      </div>
+      <Cube />
     </Container>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
