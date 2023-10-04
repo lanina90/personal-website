605d9b38dad37efad2cce211846b08033a52f142
Kateryna Laina <boba4ok@gmail.com>
2023-10-04 17:56:02 +0300

    feature: create burger menu

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,20 +2,54 @@ import React from 'react'
 import styles from './Header.module.css'
 import Container from '../Container/Container'
 import Logo from '../../assets/img/logo.svg'
-import {useMedia} from '../../hooks/useMedia';
+import { useMedia } from '../../hooks/useMedia'
 
 const Header = () => {
-  const {isMobile} = useMedia()
+  const { isMobile } = useMedia()
+
   return (
-    <Container component="header" className={styles.logo}>
+    <Container component="header" className={styles.header}>
       <img src={Logo} width={'44px'} alt="Logo" />
-
-      <nav>
-        <a href="">About</a>
-        <a href="">Experience</a>
-        <a href="">Contacts</a>
-      </nav>
-
+      {isMobile ? (
+        <>
+          <input type="checkbox" id="nav" className={styles.checkbox} />
+          <label htmlFor="nav" className={styles.button}>
+            <span className={styles.icon}>&nbsp;</span>
+          </label>
+          <div className={styles.background}>&nbsp;</div>
+          <nav className={styles.nav}>
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <a className={styles.link} href="#about">
+                  About
+                </a>
+              </li>
+              <li className={styles.item}>
+                <a className={styles.link} href="#work">Experience</a>
+              </li>
+              <li className={styles.item}>
+                <a className={styles.link} href="#contacts">Contacts</a>
+              </li>
+            </ul>
+          </nav>
+        </>
+      ) : (
+        <nav>
+          <ul className={styles.menu}>
+            <li>
+              <a href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#work">Experience</a>
+            </li>
+            <li>
+              <a href="#contacts">Contacts</a>
+            </li>
+          </ul>
+        </nav>
+      )}
     </Container>
   )
 }
--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,28 +1,31 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Cube from '../Cube/Cube'
 import styles from './Hero.module.css'
 import Container from '../Container/Container'
 
 const Hero = () => {
-
-  const [isHeadingActive, setIsHeadingActive] = useState(false);
-  const [isDescActive, setIsDescActive] = useState(false);
+  const [isHeadingActive, setIsHeadingActive] = useState(false)
+  const [isDescActive, setIsDescActive] = useState(false)
 
   useEffect(() => {
-    setIsHeadingActive(true);
+    setIsHeadingActive(true)
     setIsDescActive(true)
-  }, []);
+  }, [])
 
   return (
     <Container component={'section'} className={styles.hero}>
       <div className={styles.hero__intro}>
-        <span>Hi! My name is</span>
-        <h1 className={isHeadingActive ? `${styles.active}` : ''}>Kateryna Lanina</h1>
+        <div>
+          <h1 className={isHeadingActive ? `${styles.active}` : ''}>
+            Hi! I'm <span>Kateryna</span>
+          </h1>
+        </div>
         <p className={isHeadingActive ? `${styles.active}` : ''}>
-          I'm a web developer from Ukraine. My passion is creating websites, 3D visuals and web applications.
+          I'm a web developer from Ukraine. My passion is creating websites, 3D
+          visuals and web applications.
         </p>
       </div>
-      <Cube/>
+      <Cube />
     </Container>
   )
 }
