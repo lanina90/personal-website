2e6c5ed260c8ca737fd09985f2863e3c98abdc41
Kateryna Laina <boba4ok@gmail.com>
2023-11-09 18:04:31 +0200

    feature:  create accordion item

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import {ReactComponent as Work} from '../../assets/img/work/experience.svg';
+import {useMedia} from '../../hooks/useMedia';
 import Container from '../Container/Container';
 import Heading from '../SectionHeading/Heading';
 import AccordionItem from './Accordion/AccordionItem';
 import styles from './Experience.module.css'
 
 const Experience = () => {
+  const {isDesktop, isDeskLg} = useMedia()
   return (
     <Container component={'section'} className={styles.experience}>
       <Heading
         icon={<Work/>}
-        title={'Work history'}
+        title={(isDesktop || isDeskLg) ? 'Experience' : 'Work history'}
         text={'Featured Work and Project Showcase'}/>
     <AccordionItem/><AccordionItem/><AccordionItem/>
     </Container>
