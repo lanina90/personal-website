4310353b0b2064bd1e2e4f68d173ef014c89d1bc
Kateryna Laina <boba4ok@gmail.com>
2023-11-12 15:26:03 +0200

    feature: fill the info

--- src/Components/Skills/Technologies/Technologies.tsx
+++ src/Components/Skills/Technologies/Technologies.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {BsFiletypeCss, BsFiletypeScss} from 'react-icons/bs';
 import {DiGitBranch} from 'react-icons/di';
-import {FaReact} from 'react-icons/fa';
+import {FaCss3Alt, FaReact, FaSass} from 'react-icons/fa';
 import {IoLogoJavascript} from 'react-icons/io';
-import {SiJest, SiMui, SiTailwindcss, SiTypescript} from 'react-icons/si';
+import {SiJest, SiMui, SiRedux, SiTailwindcss, SiTypescript} from 'react-icons/si';
 import {TbBrandNextjs} from 'react-icons/tb';
+import {TiHtml5} from 'react-icons/ti';
 import styles from '../Skills.module.css';
 
 const Technologies = () => {
@@ -15,17 +15,26 @@ const Technologies = () => {
         <span>React</span>
       </div>
       <div className={styles.card}>
+        <SiRedux size={100}/>
+        <span>Redux</span>
+      </div>
+      <div className={styles.card}>
         <SiTypescript size={100}/>
         <span>TypeScript</span>
       </div>
       <div className={styles.card}>
-        <BsFiletypeCss size={140}/>
+        <FaCss3Alt size={140}/>
         <span>CSS</span>
       </div>
       <div className={styles.card}>
-        <BsFiletypeScss size={140}/>
-        <span>SCSS</span>
+        <FaSass size={140}/>
+        <span>SASS</span>
       </div>
+      <div className={styles.card}>
+        <TiHtml5 size={140}/>
+        <span>HTML5</span>
+      </div>
+
       <div className={styles.card}>
         <IoLogoJavascript size={140}/>
         <span>JavaScript</span>
